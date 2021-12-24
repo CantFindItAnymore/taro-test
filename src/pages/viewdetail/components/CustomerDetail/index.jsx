a6dda3531ad6f5c080d039c80781c2e401907073
rjx <ranjunx@qq.com>
2021-12-24 15:13:28 +0800

    feat: 客户详情完成

--- src/pages/viewdetail/components/CustomerDetail/index.jsx
+++ src/pages/viewdetail/components/CustomerDetail/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Taro from "@tarojs/taro";
 
-import { View, Image } from "@tarojs/components";
+import { View, Image, Text } from "@tarojs/components";
 import { AtTabs, AtTabsPane, AtList, AtListItem, AtTag } from "taro-ui";
 import ImageBox from "../../../../components/ImageBox";
 import styles from "./index.module.styl";
@@ -257,19 +257,19 @@ const CustomerDetail = props => {
             <AtList>
               <AtListItem
                 title="事业版块"
-                note={data[current].value.company.name}
+                note={data[current].value?.plateCode}
               />
               <AtListItem
                 title="负责人"
-                note={data[current].value.customerName}
+                note={data[current].value?.principalUserName}
               />
               <AtListItem
                 title="负责人部门"
-                note={data[current].value.company.shortName}
+                note={data[current].value?.principalOrganizationName}
               />
               <AtListItem
                 title="销售"
-                note={data[current].value.company.taxpayerTypeName}
+                note={data[current].value?.salerUserName}
               />
             </AtList>
           )}
@@ -279,231 +279,118 @@ const CustomerDetail = props => {
             <AtList>
               <AtListItem
                 title="客户评分"
-                note={data[current].value.company.name}
+                note={data[current].value?.customerScore}
               />
               <AtListItem
                 title="客户评级"
-                note={data[current].value.customerName}
+                note={data[current].value?.customerGrade}
               />
               <AtListItem
                 title="授信额度上限"
-                note={data[current].value.company.shortName}
+                note={data[current].value?.creditLimit
+                  ?.toString()
+                  ?.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               />
               <AtListItem
                 title="共享集团授信"
-                note={data[current].value.company.taxpayerTypeName}
+                note={data[current].value?.useCustomerCredit ? "是" : "否"}
               />
               <AtListItem
                 title="申请授信额度"
-                note={data[current].value.company.taxpayerNumber}
+                note={data[current].value?.applyCredit
+                  ?.toString()
+                  ?.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               />
               <AtListItem
                 title="资格认证方式"
-                note={data[current].value.company.legalPerson}
+                note={data[current].value?.validationMethodName}
               />
               <AtListItem
                 title="担保证明"
-                note={data[current].value.company.depositBank}
+                note={
+                  <ImageBox
+                    imgs={data[current].value?.guaranteeProofUrls?.split(",")}
+                  />
+                }
               />
               <AtListItem
                 title="剩余授信额度"
-                note={data[current].value.company.accountName}
+                note={data[current].value?.remainCredit
+                  ?.toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               />
             </AtList>
           )}
         </AtTabsPane>
         <AtTabsPane current={current} index={5}>
-          {current === 5 && (
-            <AtList>
-              <AtListItem
-                title="客户编号"
-                note={data[current].value.company.name}
-              />
-              <AtListItem
-                title="客户集团"
-                note={data[current].value.customerName}
-              />
-              <AtListItem
-                title="和我司合作年限"
-                note={data[current].value.company.shortName}
-              />
-              <AtListItem
-                title="同行业评价"
-                note={data[current].value.company.taxpayerTypeName}
-              />
-              <AtListItem
-                title="是否有优质产品和稳定收入"
-                note={data[current].value.company.taxpayerNumber}
-              />
-              <AtListItem
-                title="所属行业"
-                note={data[current].value.company.legalPerson}
-              />
-              <AtListItem
-                title="客户类型"
-                note={data[current].value.company.depositBank}
-              />
-              <AtListItem
-                title="授权方式"
-                note={data[current].value.company.accountName}
-              />
-              <AtListItem
-                title="主营产品"
-                note={data[current].value.company.accountNumber}
-              />
-
-              <AtListItem
-                title="邮寄地址"
-                note={data[current].value.company.foundYearName}
-              />
-              <AtListItem
-                title="月流水"
-                note={data[current].value.company.registeredCapitalName}
-              />
-              <AtListItem
-                title="合作账期"
-                note={data[current].value.company.capitalSourceName}
-              />
-              <AtListItem
-                title="优势或独代媒体签框架数量"
-                note={data[current].value.company.organizationFormsName}
-              />
-              <AtListItem
-                title="公司可把控的高层关系"
-                note={data[current].value.company.officeAreaName}
-              />
-              <AtListItem
-                title="总经理客户评分"
-                note={data[current].value.company.employeeCountName}
-              />
-              <AtListItem
-                title="以往付款态度"
-                note={data[current].value.company.legalDisputeName}
-              />
-              <AtListItem
-                title="预计年投放量"
-                note={data[current].value.companyPlate?.contactName}
-              />
-              <AtListItem
-                title="客户说明"
-                note={data[current].value.companyPlate?.contactName}
-              />
-            </AtList>
-          )}
+          {current === 5 &&
+            data[current].value?.map(_ => {
+              return (
+                <View key={_.id} className={styles.item}>
+                  <Text>媒体:{_.mediaName}</Text>
+                  <Text>媒体版块:{_.mediaSectionName}</Text>
+                  <Text>业务类型:{_.businessTypeName}</Text>
+                  <Text>客户账期:{_.customerPaymentValue}</Text>
+                  <Text>缓冲账期:{_.bufferPaymentValue}</Text>
+                  <Text>
+                    有效期:{_.startDate} - {_.endDate}
+                  </Text>
+                </View>
+              );
+            })}
         </AtTabsPane>
         <AtTabsPane current={current} index={6}>
-          {current === 6 && (
-            <AtList>
-              <AtListItem
-                title="客户编号"
-                note={data[current].value.company.name}
-              />
-              <AtListItem
-                title="客户集团"
-                note={data[current].value.customerName}
-              />
-              <AtListItem
-                title="和我司合作年限"
-                note={data[current].value.company.shortName}
-              />
-              <AtListItem
-                title="同行业评价"
-                note={data[current].value.company.taxpayerTypeName}
-              />
-              <AtListItem
-                title="是否有优质产品和稳定收入"
-                note={data[current].value.company.taxpayerNumber}
-              />
-              <AtListItem
-                title="所属行业"
-                note={data[current].value.company.legalPerson}
-              />
-              <AtListItem
-                title="客户类型"
-                note={data[current].value.company.depositBank}
-              />
-              <AtListItem
-                title="授权方式"
-                note={data[current].value.company.accountName}
-              />
-              <AtListItem
-                title="主营产品"
-                note={data[current].value.company.accountNumber}
-              />
-
-              <AtListItem
-                title="邮寄地址"
-                note={data[current].value.company.foundYearName}
-              />
-              <AtListItem
-                title="月流水"
-                note={data[current].value.company.registeredCapitalName}
-              />
-              <AtListItem
-                title="合作账期"
-                note={data[current].value.company.capitalSourceName}
-              />
-              <AtListItem
-                title="优势或独代媒体签框架数量"
-                note={data[current].value.company.organizationFormsName}
-              />
-              <AtListItem
-                title="公司可把控的高层关系"
-                note={data[current].value.company.officeAreaName}
-              />
-              <AtListItem
-                title="总经理客户评分"
-                note={data[current].value.company.employeeCountName}
-              />
-              <AtListItem
-                title="以往付款态度"
-                note={data[current].value.company.legalDisputeName}
-              />
-              <AtListItem
-                title="预计年投放量"
-                note={data[current].value.companyPlate?.contactName}
-              />
-              <AtListItem
-                title="客户说明"
-                note={data[current].value.companyPlate?.contactName}
-              />
-            </AtList>
-          )}
+          {current === 6 &&
+            data[current].value?.map(_ => {
+              return (
+                <View key={_.id} className={styles.item}>
+                  <Text>媒体:{_.mediaName}</Text>
+                  <Text>运营方式:{_.operationName}</Text>
+                  <Text>政策标签:{_.policyLableName}</Text>
+                  <Text>返点:{`${_.rebate * 100}%`}</Text>
+                  <Text>有效开始日期:{_.startDate}</Text>
+                  <Text>有效结束日期:{_.endDate}</Text>
+                  <Text>创建时间:{_.createdTime}</Text>
+                </View>
+              );
+            })}
         </AtTabsPane>
         <AtTabsPane current={current} index={7}>
           {current === 7 && (
             <AtList>
               <AtListItem
                 title="资金来源"
-                note={data[current].value.company.name}
+                note={data[current].value?.capitalSourceName}
               />
               <AtListItem
                 title="公司注册资本（实缴）"
-                note={data[current].value.customerName}
+                note={data[current].value?.registeredCapitalName
+                  ?.toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               />
               <AtListItem
-                title="公司注册形式"
-                note={data[current].value.company.shortName}
+                title="公司组织形式"
+                note={data[current].value?.organizationFormsName}
               />
               <AtListItem
                 title="客户集团核实结果"
-                note={data[current].value.company.taxpayerTypeName}
+                note={data[current].value?.customerCapitalOpinion}
               />
               <AtListItem
                 title="和我公司有过纠纷和诉讼"
-                note={data[current].value.company.taxpayerNumber}
+                note={data[current].value?.legalDisputeName}
               />
               <AtListItem
                 title="是否有外部高风险诉讼"
-                note={data[current].value.company.legalPerson}
+                note={data[current].value?.highRiskName}
               />
               <AtListItem
                 title="集团信息确认说明"
-                note={data[current].value.company.depositBank}
+                note={data[current].value?.customerInfoConfirmOpinion}
               />
               <AtListItem
                 title="审批意见"
-                note={data[current].value.company.accountName}
+                note={data[current].value?.approvalOpinion}
               />
             </AtList>
           )}
